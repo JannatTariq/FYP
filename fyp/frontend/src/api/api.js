91c0832bbc1795647438df421c3430ba3e1ea95b
FatimaTariq <l201047@lhr.nu.edu.pk>
2024-03-18 14:29:11 +0500

    nothing

--- fyp/frontend/src/api/api.js
+++ fyp/frontend/src/api/api.js
@@ -6,7 +6,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 // });
 
 const instance = axios.create({
-  baseURL: "https://db6b-2400-adc5-149-6200-25eb-3c2b-4ac3-48f.ngrok-free.app",
+  baseURL: "https://f64f-154-80-48-2.ngrok-free.app",
 });
 
 instance.interceptors.request.use(
